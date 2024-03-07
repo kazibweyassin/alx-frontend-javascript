552ab4a5e585b87ee5771d5cfe2b797e3fb484a8
kazibweyassin <kazibweusama@gmail.com>
2024-03-07 09:32:02 -0800

    Fake promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+//A fake API Promise
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
+
+export default getFullResponseFromAPI;
