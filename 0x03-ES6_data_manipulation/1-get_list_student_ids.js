c0871fb0a220036573b849f09ea692e59a63a728
kazibweyassin <kazibweusama@gmail.com>
2024-03-12 21:01:48 +0300

    fixed

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,8 @@
 export default function getListStudentIds(students) {
-    if (!Array.isArray(students)) {
-        return [];
-    }
-    let ids = [];
-    ids = students.map((x) => x.id);
-    return ids;
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  let ids = [];
+  ids = students.map((x) => x.id);
+  return ids;
 }
