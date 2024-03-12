cd18ae5fc3aa0931f8dd6b036531ca5de5dff682
kazibweyassin <kazibweusama@gmail.com>
2024-03-13 00:24:54 +0300

    tried this

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default function createInt8TypedArray(length, position, value) {
+    if (position >= length) {
+      throw Error('Position outside range');
+    }
+    const b = new ArrayBuffer(length);
+    const x = new Int8Array(b, 0, length);
+    x.set([value], position);
+    return new DataView(b);
+  }
