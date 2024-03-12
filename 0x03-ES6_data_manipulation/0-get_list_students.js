dec622698d4502f1fcfbdc57525599de9583a411
kazibweyassin <kazibweusama@gmail.com>
2024-03-12 14:50:12 +0300

    map

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,10 @@
-import axios from 'axios';
-
 export default function getListStudents() {
-
+  // Array of student objects
   const students = [
     { id: 1, firstName: "Guillaume", location: "San Francisco" },
     { id: 2, firstName: "James", location: "Columbia" },
     { id: 5, firstName: "Serena", location: "San Francisco" },
   ];
-
+  // Returning the array of students
   return students;
 }
