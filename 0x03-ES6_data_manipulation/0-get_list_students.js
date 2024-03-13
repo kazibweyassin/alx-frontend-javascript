d52a047348382e2401fe7bfb81bdf370a82affed
kazibweyassin <kazibweusama@gmail.com>
2024-03-13 07:52:37 +0300

    another try

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,6 @@
 export default function getListStudents() {
-  const students = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-  return students;
+  const g = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const j = { id: 2, firstName: 'James', location: 'Columbia' };
+  const s = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  return [g, j, s];
 }
