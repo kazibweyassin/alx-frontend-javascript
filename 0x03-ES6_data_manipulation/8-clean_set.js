3e28591617eefb81195bde5acd40bd59e0ec8d7f
kazibweyassin <kazibweusama@gmail.com>
2024-03-15 03:21:02 -0700

    lint

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,13 @@
 function cleanSet(set, startString) {
-    let str = []; // Declaring str outside of the if block
-    if (typeof startString == 'string' && startString !== '') {
-        for (const x of set) {
-            if (x.indexOf(startString) == 0) {
-                str.push(x.slice(startString.length));
-            }
-        }
+  const str = []; // Declaring str outside of the if block
+  if (typeof startString === 'string' && startString !== '') {
+    for (const x of set) {
+      if (x.indexOf(startString) === 0) {
+        str.push(x.slice(startString.length));
+      }
     }
-    return str.join('-');
+  }
+  return str.join('-');
 }
 
 export default cleanSet;
